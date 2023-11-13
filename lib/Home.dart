dc5913c86e3eb874b011de70867701e4f6f08ec5
MinhHauNguyen <hau95024@donga.edu.vn>
2023-11-13 23:10:33 +0700

    first commit

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,7 +1,35 @@
-import 'package:demo/Login.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'dart:convert';
+
+void main() {
+  runApp(MaterialApp(home: Login()));
+}
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => Home(userList: {
+                        'lastname': 'Doe',
+                        'firstname': 'John',
+                        'mobile': '123456789',
+                        'email': 'john@example.com'
+                      })),
+            );
+          },
+          child: Text('Login'),
+        ),
+      ),
+    );
+  }
+}
 
 class Home extends StatefulWidget {
   final userList;
@@ -16,9 +44,9 @@ class _HomeState extends State<Home> {
   late String firstname;
   late String mobile;
   late String email;
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
     Map<String, dynamic> userdata = widget.userList;
     lastname = userdata['lastname'];
@@ -30,6 +58,15 @@ class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.popUntil(context, ModalRoute.withName('/'));
+          },
+        ),
+      ),
       body: Padding(
         padding: EdgeInsets.all(8.0),
         child: SingleChildScrollView(
@@ -37,40 +74,37 @@ class _HomeState extends State<Home> {
             padding: const EdgeInsets.all(20),
             child: Column(
               children: [
-                SizedBox(
-                  height: 20,
-                ),
+                SizedBox(height: 20),
                 Container(
-                  // alignment: Alignment.topLeft,
                   child: Text(
-                    'Welcome,' + firstname + ' ' + lastname,
+                    'Welcome, $firstname $lastname',
                     style: TextStyle(fontSize: 24, color: Colors.green),
                   ),
                 ),
-                SizedBox(
-                  height: 30,
+                SizedBox(height: 30),
+                Container(
+                  child: OutlinedButton.icon(
+                    onPressed: () {},
+                    icon: Icon(Icons.phone),
+                    label: Text(mobile),
+                  ),
                 ),
                 Container(
                   child: OutlinedButton.icon(
-                      onPressed: () {},
-                      icon: Icon(Icons.phone),
-                      label: Text(mobile)),
+                    onPressed: () {},
+                    icon: Icon(Icons.email),
+                    label: Text(email),
+                  ),
                 ),
                 Container(
                   child: OutlinedButton.icon(
-                      onPressed: () {},
-                      icon: Icon(Icons.email),
-                      label: Text(email)),
-                ),
-                Container(
-                  child: OutlinedButton.icon(
-                      onPressed: () {
-                        logout();
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => Login()));
-                      },
-                      icon: Icon(Icons.logout),
-                      label: Text('Logout')),
+                    onPressed: () {
+                      logout();
+                      Navigator.pop(context); // Đóng màn hình hiện tại
+                    },
+                    icon: Icon(Icons.logout),
+                    label: Text('Logout'),
+                  ),
                 ),
                 SizedBox(height: 20),
               ],
@@ -84,18 +118,16 @@ class _HomeState extends State<Home> {
   Future<void> logout() async {
     try {
       Response response = await get(
-        // Uri.parse('https://dummyjson.com/auth/login'),
         Uri.parse(
             'https://e-commerce-backend-u0i4.onrender.com/api/user/logout'),
       );
       Data data = Data.fromJson(jsonDecode(response.body.toString()));
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text(
-          data.mess,
-        ),
+        content: Text('back'),
         backgroundColor: Colors.red,
       ));
-      print(data.mess);
+      Navigator.popUntil(
+          context, ModalRoute.withName('/')); // Quay lại màn hình đầu tiên
     } catch (e) {
       print(e.toString());
     }
